082b872265c39f119e7e5f6e15d244313aee10dd
Kat <kyevsukov@gmail.com>
2021-08-10 18:10:45 -0400

    finished MVP and stretch 1, working on stretch 2

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -4,13 +4,26 @@ import './Comments.css';
 
 const Comments = props => {
   // 🔥 Make sure the parent of Comments is passing the right props!
-  const { comments } = props;
+  const { comments, setComment } = props;
 
+  // const handleComment = event => {
+  //   setComment(input.value)
+  // }
+  
   return (
     <div>
       {/* map through the comments prop and render a Comment for every piece of data */}
+      {comments.map(comment => <Comment comment={comment}/> )}
+      <form><input type="text" placeholder='Leave a comment..'></input>
+      <button>Comment</button>
+      </form>
     </div>
   );
 };
 
 export default Comments;
+
+//setcomment, needs to be passed into comment
+//button onclick needs a change handler that sets puts typed comment into state
+// value={this.state.value}
+// onClick={handleComment
